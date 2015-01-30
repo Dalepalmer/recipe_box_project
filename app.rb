bf168a7493e1be7b4354907a85e8b9ab11e2d560
Ron Park & Dale Palmer <student@epicodus.com>
2015-01-29 16:40:07 -0800

    search through ingredients listed in all recipes

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get('/') do
   @recipes = Recipe.all()
   @ingredients = Ingredient.all()
+  @categories = Category.all()
   erb(:index)
 end
 
@@ -23,6 +24,13 @@ post('/ingredients') do
   redirect '/'
 end
 
+post('/category') do
+  @categories = Category.all()
+  name = params.fetch("category_name")
+  Category.create({:name => name})
+  redirect '/'
+end
+
 post('/ingredients_recipes_add') do
   recipe_id = params.fetch("recipe_id")
   @recipe = Recipe.find(recipe_id)
@@ -33,9 +41,36 @@ post('/ingredients_recipes_add') do
   redirect '/'
 end
 
+post('/categories_recipes_add') do
+  category_id = params.fetch("category_id")
+  @categories = Category.find(category_id)
+  recipe_id = params.fetch("recipe_id")
+  @recipe = Recipe.find(recipe_id)
+  @recipe.categories().<<(Category.find(category_id))
+  redirect '/'
+end
+
+post("/recipes") do
+  name = params.fetch("name")
+  @recipe = Recipe.new({:name => name})
+  @recipe.save()
+  if @recipe.save()
+    redirect '/'
+  else
+    erb(:errors)
+  end
+end
+
 get('/recipes/:id') do
   recipe_id = params.fetch("id").to_i
   @recipe = Recipe.find(recipe_id)
   @ingredients = @recipe.ingredients()
+  @categories = @recipe.categories()
   erb(:recipe)
+end
+
+get('/ingredients/:id') do
+  ingredient_id = params.fetch("id").to_i
+  @ingredient = Ingredient.find(ingredient_id)
+  erb(:ingredient)
 end
--- lib/recipe.rb
+++ lib/recipe.rb
@@ -2,4 +2,12 @@ class Recipe < ActiveRecord::Base
   has_and_belongs_to_many(:ingredients)
   has_and_belongs_to_many(:categories)
   has_one(:instruction)
+
+  validates(:name, :presence => true)
+
+  # define_singleton_method("search") do |ingredient_id|
+  #   @recipe = Recipe.all()
+  #   @ingredient = Ingredient.find(ingredient_id)
+  #
+
 end
